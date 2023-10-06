c8a062f3e0ac4cfbacd880ccc305e76a7b93f3af
demo <duongdtph27826@fpt.edu.vn>
2023-10-06 20:46:28 +0700

    LoginScreen

--- MainApp/E_tech_App/App.js
+++ MainApp/E_tech_App/App.js
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './Login';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App=()=> {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+        name='Login'
+        component={Login}
+        options={{
+          headerShown:false,
+        }}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
