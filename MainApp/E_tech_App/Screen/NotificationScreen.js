b9c179c881bc5eef032154bbbe1b241647e71a0c
nphat24 <106876162+nphat24@users.noreply.github.com>
2023-10-09 23:07:36 +0700

    Bottom tabs Navigation

---
+++ MainApp/E_tech_App/Screen/NotificationScreen.js
@@ -0,0 +1,11 @@
+import { View, Text , FlatList} from 'react-native'
+import React from 'react'
+
+const NotificationScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Text>NotificationScreen</Text>
+    </View>
+  )
+}
+export default NotificationScreen;
