d040a0d5d56e6ea4d6a608aff2a1285afff5fa6d
TungDuong <duongdtph27826@fpt.edu.vn>
2023-10-18 22:22:04 +0700

    fix giao diện đăng ký đăng nhập

--- MainApp/E_tech_App/App.js
+++ MainApp/E_tech_App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
 import Home from './Screen/home/Home';
 import Profile from './Screen/profile/profileScreen';
 import ChiTietSP from './Screen/ChiTietSP';
@@ -10,8 +11,25 @@ import Login from './Screen/Login';
 import SignUp from './Screen/SignUp';
 import editProfile from './Screen/profile/editPrifile';
 
-const Stack = createNativeStackNavigator();
+import index from './Screen/navigation';
+import CartScreen from './Screen/CartScreen';
+import NotificationScreen from './Screen/NotificationScreen';
+import AccountScreen from './Screen/AccountScreen';
 
+// Navigation Import
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AnimatedTabBarNavigator } from "react-native-animated-nav-tab-bar";
+import Icon from 'react-native-vector-icons/Feather';
+
+const Stack = createNativeStackNavigator();
+const Tabs = AnimatedTabBarNavigator();
+const screenOptions = {
+  activeTintColor: "#2F7C6E",
+  inactiveTintColor: "#222222",
+  activeBackgroundColor: "blue"
+}
 const App = () => {
   return (
     <NavigationContainer>
@@ -26,6 +44,73 @@ const App = () => {
         <Stack.Screen name='SignUp' component={SignUp} options={{ headerShown: false }} />
         <Stack.Screen name='EditProfile' component={editProfile} options={{ headerShown: false }} />
       </Stack.Navigator>
+      <Tabs.Navigator
+        // default configuration from React Navigation
+        tabBarOptions={{
+          screenOptions
+        }}
+      >
+
+        <Tabs.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+                <Icon
+                    name="home"
+                    size={size ? size : 24}
+                    color={focused ? color : "#222222"}
+                    focused={focused}
+                />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="Cart"
+          component={CartScreen}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <Icon
+                name="shopping-cart"
+                size={size ? size : 24}
+                color={focused ? color : "#222222"}
+                focused={focused}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="Notification"
+          component={NotificationScreen}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <Icon
+                name="bell"
+                size={size ? size : 24}
+                color={focused ? color : "#222222"}
+                focused={focused}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="Account"
+          component={AccountScreen}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <Icon
+                name="user"
+                size={size ? size : 24}
+                color={focused ? color : "#222222"}
+                focused={focused}
+              />
+            )
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 }
