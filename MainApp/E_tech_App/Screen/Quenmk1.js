2a8d3ef6f1973b3af68c86fbce58e884d8b5a5a2
nphat24 <106876162+nphat24@users.noreply.github.com>
2023-10-26 10:27:06 +0700

    fixx 2

--- MainApp/E_tech_App/Screen/Quenmk1.js
+++ MainApp/E_tech_App/Screen/Quenmk1.js
@@ -8,7 +8,7 @@ import {
   View,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { isValidEmail } from "../../Component/validation";
+import { isValidEmail } from "../Component/validation";
 
 
 const Quenmk1 = (navigation) => {
