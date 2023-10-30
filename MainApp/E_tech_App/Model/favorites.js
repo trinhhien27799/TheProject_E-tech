2e6091a19fd0d53862a83a7ee4bb4d7ea174230e
nphat24 <106876162+nphat24@users.noreply.github.com>
2023-10-30 20:55:17 +0700

    favorite

---
+++ MainApp/E_tech_App/Model/favorites.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const favorites = () => {
+  const [favorite, setFavorite] = useState(null);
+  useEffect(() => {
+    axios.get('http://192.168.110.173:3000/api/favorite/get-all').then((res) => {
+      setFavorite(res.data);
+    })
+  }, [])
+
+  return favorites;
+}
