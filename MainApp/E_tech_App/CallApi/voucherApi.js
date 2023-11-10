a32176b1f8ed58da9da81213e7af51c7601a5913
nphat24 <106876162+nphat24@users.noreply.github.com>
2023-11-11 00:23:52 +0700

    API Voucher

---
+++ MainApp/E_tech_App/CallApi/voucherApi.js
@@ -0,0 +1,73 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API_BASE_URL } from "./config";
+export const getAllVoucher = async () => {
+    const username = await AsyncStorage.getItem('username');
+    try {
+        const response = await fetch(`${API_BASE_URL}/api/voucher/get-all`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username:username})
+        });
+        const data = await response.json();
+        console.log(data);
+        if (data.code == 200) {
+            alert('Lấy giỏ hàng thành công')
+        } else
+        alert (data.message);
+        return data;
+    } catch (error) {
+        console.error('Lỗi yêu cầu mạng:', error);
+        throw error;
+    }
+}
+
+
+
+export const addVoucher = async (voucherCode , voucherID) => {
+    const username = await AsyncStorage.getItem('username');
+    try {
+        const response = await fetch(`${API_BASE_URL}/api/voucher/add`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username:username,voucherCode: voucherCode,voucherID: voucherID})
+        });
+        const data = await response.json();
+        console.log(data);
+        if (data.code == 200) {
+            alert('Thêm voucher thành công')
+        } else
+        alert (data.message);
+        return data;
+
+        
+    } catch (error) {
+        console.error('Lỗi yêu cầu mạng:', error);
+        throw error;
+    }
+}
+export const getMyVoucher = async () => {
+    const username = await AsyncStorage.getItem('username');
+    try {
+        const response = await fetch(`${API_BASE_URL}/api/voucher/get`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username:username})
+        });
+        const data = await response.json();
+        console.log(data);
+        if (data.code == 200) {
+            alert('Lấy giỏ hàng thành công')
+        } else
+        alert (data.message);
+        return data;
+    } catch (error) {
+        console.error('Lỗi yêu cầu mạng:', error);
+        throw error;
+    }
+}
