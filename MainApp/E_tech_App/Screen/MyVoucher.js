7ccf1da0e01623058887deab08602f744e64e2ed
trinhhien27799 <117814072+trinhhien27799@users.noreply.github.com>
2023-11-14 20:29:45 +0700

    Voucher_Change

--- MainApp/E_tech_App/Screen/MyVoucher.js
+++ MainApp/E_tech_App/Screen/MyVoucher.js
@@ -10,12 +10,13 @@ import {
   SafeAreaView,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { vouchers } from '../../Model/voucher';
-import { addVoucher } from '../../CallApi/voucherApi';
+import { myvouchers } from '../Model/voucher';
 import { useNavigation } from '@react-navigation/native';
 
-export default function Makhuyenmai() {
+
+export default function MyVoucher() {
   const navigation = useNavigation();
+
 
   // const data = [
   //   {
@@ -75,55 +76,17 @@ export default function Makhuyenmai() {
   //   },
   // ];
 
-  const [voucherCode, setVoucherCode] = useState('');
-  const [voucherID, setVoucherID] = useState('');
-
-  const handleVoucher = async () => {
-    try {
-      addVoucher(voucherCode, voucherID);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  }
-
-  const renderItemVoucher = ( item) => (
-    <View style={styles.view2}>
-      <View
-        style={{
-          width: '28%',
-          paddingTop: 10,
-          borderColor: 'black',
-          borderRightWidth: 1,
-          alignContent: 'center',
-          justifyContent: 'center',
-        }}>
-        <Image source={require('../../img/sale.png')} style={styles.img} />
-      </View>
-      <View style={{ paddingTop: 10, width: '50%', marginLeft: 10 }}>
-        <Text style={styles.title} >{item.description}</Text>
-        <Text >Đơn tối thiểu {item.condition}.000đ</Text>
-        <Text style={styles.title2}>HSD: {item.expiration_date}</Text>
-      </View>
-      <View style={{ paddingTop: 10, alignContent: 'center', justifyContent: 'center' }}>
-        <TouchableOpacity style={styles.button2} onPress={handleVoucher}>
-          <Text style={{ color: 'white', fontSize: 16 }}>Lưu</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-
-
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.view}>
         <TouchableOpacity onPress={() => { navigation.goback() }}>
           <Ionicons name="arrow-back" size={30} color="black" />
         </TouchableOpacity>
-        <Text style={styles.text}>Voucher của Shop</Text>
+        <Text style={styles.text}>Voucher của bạn</Text>
       </View>
       <View>
         <FlatList
-          data={vouchers}
+          data={myvouchers}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <View style={styles.view2}>
@@ -162,7 +125,7 @@ const styles = StyleSheet.create({
     height: 30,
   },
   text: {
-    marginLeft: 60,
+    marginLeft: 90,
     fontSize: 18,
   },
   view2: {
@@ -178,9 +141,9 @@ const styles = StyleSheet.create({
   },
   img: {
     height: 80,
-    width: 90,
-    marginLeft: 6,
-    marginBottom: 8,
+    width: 100,
+    marginBottom: 10,
+    marginLeft: 10
   },
   title: {
     fontSize: 16,
@@ -189,14 +152,5 @@ const styles = StyleSheet.create({
     fontSize: 13,
     marginTop: 10,
   },
-  button2: {
-    backgroundColor: '#336BFA',
-    height: 60,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
-    padding: 10,
-    marginTop: 10,
-  },
+
 });
