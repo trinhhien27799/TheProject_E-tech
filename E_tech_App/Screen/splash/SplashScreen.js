f975771505e0c965443aa3e1d4d0097fb5747fe2
TungDuong <duongdtph27826@fpt.edu.vn>
2023-11-19 15:52:04 +0700

    tesst

--- E_tech_App/Screen/splash/SplashScreen.js
+++ E_tech_App/Screen/splash/SplashScreen.js
@@ -2,13 +2,9 @@ import React, { useEffect } from 'react'
 import { View, Text, StyleSheet, SafeAreaView, StatusBar } from 'react-native'
 import LottieView from 'lottie-react-native'
 import { autoLogin } from '../../CallApi/authenApi'
-<<<<<<<<< Temporary merge branch 1
 import { getUser, setUser } from '../../session'
 import { useNavigation } from '@react-navigation/native';
-=========
-import { setUser } from '../../session'
-import { useNavigation } from '@react-navigation/native'
->>>>>>>>> Temporary merge branch 2
+
 
 const SplashScreen = () => {
     const navigation = useNavigation()
@@ -22,13 +18,10 @@ const SplashScreen = () => {
         } catch (error) {
             console.log(`splash :${error}`)
         } finally {
-<<<<<<<<< Temporary merge branch 1
             const user = getUser();
             console.log(user);
             navigation.navigate('ButtonNavigation',{registrationData:user})
-=========
-            navigation.replace('Home')
->>>>>>>>> Temporary merge branch 2
+
         }
     }
     useEffect(() => {
